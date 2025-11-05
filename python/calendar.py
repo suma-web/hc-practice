b8a6fae7287f4a0bf8e2760c57fcf4755f634028
sample tarou <sample@example.com>
2025-11-05 21:43:23 +0900

    コードの修正

--- python/calendar.py
+++ python/calendar.py
@@ -2,40 +2,43 @@ import sys
 import datetime
 
 args = sys.argv
-t_w_name = ["月","火","水","木","金","土","日"]
-t_m = datetime.date.today().month
-t_y = datetime.date.today().year
+weekday_names = ["月","火","水","木","金","土","日"]
+today = datetime.date.today()
+current_month = today.month
+current_year = today.year
 
-def t_m_days(y,m):
-    t_m_firtst = datetime.datetime(y,m,1)
+def current_month_days(y, m):
+    current_month_firtst = datetime.datetime(y, m, 1)
     if m == 12:
-        t_m_last = datetime.datetime(y+1,1,1)
+        current_month_last = datetime.datetime(y+1, 1, 1)
     else:
-        t_m_last = datetime.datetime(y,m+1,1)
+        current_month_last = datetime.datetime(y, m+1, 1)
 
-    return (t_m_last - t_m_firtst).days
+    return (current_month_last - current_month_firtst).days
 
 if len(args) == 3 and args[1] == "-m":
-    new_t_m = int(args[2])
     try:
-        t_m_days(2025, new_t_m)
+        new_current_month = int(args[2])
+        current_month_days(2025, new_current_month)
     except ValueError:
-        print(f"{new_t_m} is neither a month number (1..12) nor a name")
+        print(f"{args[2]} is neither a month number (1..12) nor a name")
+        sys.exit(1)
+    except TypeError:
+        print(f"{args[2]} is not entered in the correct format")
         sys.exit(1)
     else:
-        t_m = new_t_m
+        current_month = new_current_month
 
-today = datetime.date.today()
-highlight_day = today.day if (today.year == t_y and today.month == t_m) else None
+highlight_day = today.day if (today.year == current_year and today.month == current_month) else None
 
-first_day = datetime.date(t_y, t_m, 1)
+first_day = datetime.date(current_year, current_month, 1)
 start_weekday = first_day.weekday()
 
-print(f"{t_m:8}月 {t_y}")
-print(" ".join(t_w_name)) 
+print(f"{current_month:8}月 {current_year}")
+print(" ".join(weekday_names)) 
 print("   " * start_weekday, end="")
 
-for day in range(1, t_m_days(t_y, t_m) + 1):
+for day in range(1, current_month_days(current_year, current_month) + 1):
     if day == highlight_day:
         cell = f"[{day}]"
         print(cell, end="")
